d1d69696d314885c4e763c8670dd03340445c68a
marloesverdonk <marloesverdonk@live.nl>
2019-08-07 12:24:09 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,21 @@
 function submitComment(){
-console.log("Hello")
+const inputField = document.getElementById('name');
+const name = inputField.value
+console.log(name);
+
+const messageField = document.getElementById('msg');
+const msg = messageField.value
+console.log(msg);
+
+const comment = document.createElement('section');
+const h3 = document.createElement('h3');
+const p = document.createElement("p");
+
+h3.innerHTML = '${name} said:';
+p.innerHTML = msg.value;
+comment.classList.add('comment')
+comment.appendChild(h3)
+comment.appendChild(p)
+
+console.log(comment)
 }
