282a949373476819f4c9d9789f5bfc63063e22cd
marloesverdonk <marloesverdonk@live.nl>
2019-08-07 15:23:24 +0200

    value null

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -11,11 +11,19 @@ const comment = document.createElement('section');
 const h3 = document.createElement('h3');
 const p = document.createElement("p");
 
-h3.innerHTML = '${name} said:';
-p.innerHTML = msg.value;
+h3.innerHTML = `${name} said:`;
+p.innerHTML = msg;
 comment.classList.add('comment')
 comment.appendChild(h3)
 comment.appendChild(p)
 
 console.log(comment)
+
+const commentSection = document.getElementById('comments')
+console.log(commentSection)
+
+commentSection.appendChild(comment)
+
+inputField.value = null
+messageField.value = null
 }
