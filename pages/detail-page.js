f55a7031081e9ec415aa695b79d1473e4e13a994
marloesverdonk <marloesverdonk@live.nl>
2019-08-08 14:42:37 +0200

    Set up js files and declare submitComment

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,49 +1,63 @@
-function doesNotPassValidations(name, msg){
-    if(!name || !msg){
-        alert('You forgot to fill in your name or message!')
+function doesNotPassValidations(name, msg) {
+
+    if (!name && !msg) {
+        alert('You forgot to fill in your name and message!')
         return true
     }
-    
-    if(msg.length > 10){
+    if (!name) {
+        alert('You forgot to fill in your name!')
+        return true
+    }
+
+    if (!msg) {
+        alert('You forgot to fill in your message!')
+        return true
+    }
+
+
+    if (msg.length > 10) {
         alert('You used too many characters')
         return true
     }
 
+
     return false
 }
 
-function submitComment(){
-     
-const inputField = document.getElementById('name');
-const name = inputField.value
-console.log(name);
+function submitComment() {
 
-const messageField = document.getElementById('msg');
-const msg = messageField.value
-console.log(msg);
+    const inputField = document.getElementById('name');
+    const name = inputField.value
+    const nameCa = name[0].toUppercase()
 
-if (doesNotPassValidations(name, msg)){
-    return null
-}
+    console.log(name);
+
+    const messageField = document.getElementById('msg');
+    console.log(msg);
+
+    if (doesNotPassValidations(name, msg)) {
+        return null
+    }
+   
+    
+
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement("p");
+
+    h3.innerHTML = `${name} said:`;
+    p.innerHTML = msg;
+    comment.classList.add('comment')
+    comment.appendChild(h3)
+    comment.appendChild(p)
 
 
-const comment = document.createElement('section');
-const h3 = document.createElement('h3');
-const p = document.createElement("p");
+    const commentSection = document.getElementById('comments')
+    console.log(commentSection)
 
-h3.innerHTML = `${name} said:`;
-p.innerHTML = msg;
-comment.classList.add('comment')
-comment.appendChild(h3)
-comment.appendChild(p)
+    commentSection.appendChild(comment)
 
-
-const commentSection = document.getElementById('comments')
-console.log(commentSection)
-
-commentSection.appendChild(comment)
-
-inputField.value = null
-messageField.value = null
+    inputField.value = null
+    messageField.value = null
 
 }
